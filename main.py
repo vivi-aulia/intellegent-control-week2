da8af3bbdcbe39cf91a0b4fcd28622701abe22ef
vivi-aulia <auliavivi074@gmail.com>
2025-02-22 16:00:32 +0700

    menambahkan file

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import cv2
 import joblib
-import numpy as np
+import numpy as np 
 
 # Muat model KNN dan scaler
 knn = joblib.load('model_knn.pkl')
 scaler = joblib.load('scaler.pkl')
 
 # Inisialisasi kamera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
-
-    #Ambil pixel tengah gambar
+    
+    # Ambil pixel tengah gambar
     height, width, _ = frame.shape
     pixel_center = frame[height//2, width//2]
-
+    
     # Normalisasi pixel sebelum prediksi
     pixel_center_scaled = scaler.transform([pixel_center])
-
+    
     # Prediksi warna
     color_pred = knn.predict(pixel_center_scaled)[0]
-
-   # Tampilkan warna pada frame
+    
+    # Tampilkan warna pada frame
     cv2.putText(frame, f'Color: {color_pred}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
     
     cv2.imshow('Frame', frame)
